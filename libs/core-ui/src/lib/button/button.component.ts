866b5a749d9c274cfffa2883ccab0eb20d176478
Chris Bautista <chrisbautistaaa@gmail.com>
2022-08-30 23:21:27 -0700

    feat: add button

--- libs/core-ui/src/lib/button/button.component.ts
+++ libs/core-ui/src/lib/button/button.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'core-ui-button',
   templateUrl: './button.component.html',
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Input() text!: string;
+  @Input() variant: 'primary' | 'secondary' = 'primary';
+}
