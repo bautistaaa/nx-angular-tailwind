e34264b0a1a12b18a690e4dbb82ceee9e35757ef
Chris Bautista <chrisbautista@netflix.com>
2022-08-31 16:58:28 -0700

    feat: darkmode support

--- libs/core-ui/src/lib/header/header.component.ts
+++ libs/core-ui/src/lib/header/header.component.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'core-ui-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   items = [
     { name: 'Dashboard', href: '#', current: true },
     { name: 'Team', href: '#', current: false },
     { name: 'Projects', href: '#', current: false },
     { name: 'Calendar', href: '#', current: false },
   ];
+
+  isDark = false;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.isDark =
+      localStorage.getItem('theme') === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+    this.toggleClasses();
+  }
+
+  toggleTheme(): void {
+    if (this.isDark) {
+      localStorage.setItem('theme', 'light');
+      this.isDark = false;
+    } else {
+      this.isDark = true;
+      localStorage.setItem('theme', 'dark');
+    }
+
+    this.toggleClasses();
+  }
+
+  toggleClasses(): void {
+    if (this.isDark) {
+      this.renderer.addClass(document.getElementById('html'), 'dark');
+    } else {
+      this.renderer.removeClass(document.getElementById('html'), 'dark');
+    }
+  }
 }
